c528ffdc9ba86e12f89dd0d22aa0db6c4dc78f08
DEBASHIS DEY <ddey23006@gmail.com>
2024-11-17 14:27:23 +0530

    large h5 file to github

---
+++ main.py
@@ -0,0 +1,70 @@
+# Library imports
+import numpy as np
+import streamlit as st
+import cv2
+from tensorflow.keras.models import load_model
+
+# Loading the Model
+model = load_model(
+    'C:/Users/karlo/Documents/PHD project Tea disease streamlit app/Tea_disease.h5', compile=False)
+
+# Name of Classes
+CLASS_NAMES = ['Good_Leaf', 'Red_Rust', 'Red_Spider_Mite', 'Brown_Blight']
+
+# Disease Information
+disease_info = {
+    'Good_Leaf': """
+        **This is a Non-diseased tea leaf**
+    """,
+    'Red_Rust': """
+        **Description :** 
+        
+        Red rust is a common disease of tea plants...
+    """,
+    'Red_Spider_Mite': """
+        **Description :** 
+        
+        Red spider mites are common pests...
+    """,
+    'Brown_Blight': """
+        **Description :** 
+        
+        Brown blight is a common disease of tea plants...
+    """
+}
+
+# Setting Title of App
+st.markdown('<p style="font-size:24px;"><b>Tea Disease Detection (A Plant Disease Detection Tool)</b></p>', unsafe_allow_html=True)
+st.markdown('<p style="font-size:18px;"><b>Upload an image of the plant leaf</b></p>', unsafe_allow_html=True)
+
+st.markdown('<p style="font-size:18px;">Choose an image...</p>', unsafe_allow_html=True)
+
+# Uploading the Plant image
+plant_image = st.file_uploader("", type=["jpeg", "jpg", "png"])
+submit = st.button('Predict')
+
+# On predict button click
+if submit:
+    if plant_image is not None:
+        # Convert the file to an OpenCV image
+        file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+
+        # Displaying the image
+        st.image(opencv_image, channels="BGR")
+        st.write(opencv_image.shape)
+
+        # Resizing and normalizing the image
+        opencv_image = cv2.resize(opencv_image, (512, 512))
+        opencv_image = opencv_image / 255.0  # Normalization
+
+        # Convert image to 4 Dimensions
+        opencv_image = np.expand_dims(opencv_image, axis=0)
+
+        # Make Prediction
+        Y_pred = model.predict(opencv_image)
+        result = CLASS_NAMES[np.argmax(Y_pred)]
+
+        # Display disease information
+        st.markdown(f'<p style="font-size:22px;"><b>This is a tea leaf with {result}</b></p>', unsafe_allow_html=True)
+        st.markdown(disease_info[result])
